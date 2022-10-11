4b08bb7dd49cd48dc9fffc6b0ffade7e02805865
Arnau <arnau.rivas21@gmail.com>
2022-10-11 18:50:20 +0200

    First Commit

---
+++ UniversalPlayer/UniversalPlayer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UniversalPlayer
+//
+//  Created by Arnau Rivas Rivas on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UniversalPlayer/UniversalPlayer/UniversalPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalPlayerApp.swift
+//  UniversalPlayer
+//
+//  Created by Arnau Rivas Rivas on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UniversalPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
